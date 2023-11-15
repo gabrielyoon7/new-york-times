e58308356bcc0de96a43f52f1b4a73e71ecc4b81
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-11-15 23:49:22 +0900

    chore: vite svgr 설정 및 아이콘 추가

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,5 +1,7 @@
 import { Outlet, useNavigate } from 'react-router-dom';
 import { Container } from './Layout.style.ts';
+import HomeIcon from '@assets/icon-home.svg?react';
+import ScrappedIcon from '@assets/icon-scrapped.svg?react';
 
 function Layout() {
   const navigate = useNavigate();
@@ -10,8 +12,14 @@ function Layout() {
         <Outlet />
       </div>
       <div>
-        <button onClick={() => navigate('/')}>Home</button>
-        <button onClick={() => navigate('/scrapped')}>Scrapped</button>
+        <button onClick={() => navigate('/')}>
+          <HomeIcon />
+          Home
+        </button>
+        <button onClick={() => navigate('/scrapped')}>
+          <ScrappedIcon />
+          Scrapped
+        </button>
       </div>
     </Container>
   );
