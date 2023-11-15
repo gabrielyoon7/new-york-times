0b015ff064c7cde0932014b9742417b06ef26dc4
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-11-15 23:27:29 +0900

    chore: vite 절대 경로 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Layout from './components/layout/Layout.tsx';
-import Home from './pages/Home.tsx';
-import Scrapped from './pages/Scrapped.tsx';
 import { GlobalStyle } from './styles/GlobalStyle.ts';
+import Home from '@pages/Home.tsx';
+import Scrapped from '@pages/Scrapped.tsx';
+import Layout from '@components/layout/Layout.tsx';
 
 const router = createBrowserRouter(
   [
