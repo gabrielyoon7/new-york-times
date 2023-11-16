69357e30643d882114fb0a8b5b0a7349c881d4e6
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-11-16 15:07:55 +0900

    feat: 모달 관련 recoil 코드 추가

---
+++ src/recoil/modalOpenState.ts
@@ -0,0 +1,29 @@
+import { atom, selector } from 'recoil';
+import { ReactNode } from 'react';
+
+export const modalOpenState = atom<boolean>({
+  key: 'modalOpenState',
+  default: false,
+});
+
+export const modalContentState = atom<ReactNode>({
+  key: 'modalContentState',
+  default: null,
+});
+
+export const modalOpenRepository = selector({
+  key: 'modalOpenRepository',
+  get: ({ getCallback }) => {
+    const openModal = getCallback(({ set }) => async (content: ReactNode) => {
+      set(modalContentState, content);
+      set(modalOpenState, true);
+    });
+
+    const closeModal = getCallback(({ set }) => async () => {
+      set(modalContentState, null);
+      set(modalOpenState, false);
+    });
+
+    return { openModal, closeModal };
+  },
+});
