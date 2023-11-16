a15622470de5f9e2fb92e77bca540a24c6401df4
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-11-16 15:28:12 +0900

    fix: 이벤트 버블링 방지

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,15 +1,20 @@
 import { useRecoilValue } from 'recoil';
 import { modalContentState, modalOpenRepository, modalOpenState } from '@recoil/modalOpenState.ts';
 import { StyledModalBackdrop, StyledModalContent } from '@components/modal/Modal.styles.ts';
+import { MouseEvent } from 'react';
 
 function Modal() {
   const isModalOpen = useRecoilValue(modalOpenState);
   const modalContent = useRecoilValue(modalContentState);
   const { closeModal } = useRecoilValue(modalOpenRepository);
 
+  const handleClickModalContent = (event: MouseEvent<HTMLDivElement>) => {
+    event.stopPropagation();
+  };
+
   return (
     <StyledModalBackdrop isOpen={isModalOpen} onClick={closeModal}>
-      <StyledModalContent>{modalContent}</StyledModalContent>
+      <StyledModalContent onClick={handleClickModalContent}>{modalContent}</StyledModalContent>
     </StyledModalBackdrop>
   );
 }
