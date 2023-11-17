c37f4ea3690e00cc77fc0037c9732fa683b433f8
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-11-17 15:14:38 +0900

    style: prettier 재적용

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,10 +1,10 @@
-import {Outlet} from 'react-router-dom';
-import {StyledContainer} from './Layout.styles.ts';
+import { Outlet } from 'react-router-dom';
+import { StyledContainer } from './Layout.styles.ts';
 import Navbar from '@components/layout/Navbar.tsx';
-import {useRecoilValue} from 'recoil';
-import {modalOpenRepository} from '@recoil/modalOpenState.ts';
+import { useRecoilValue } from 'recoil';
+import { modalOpenRepository } from '@recoil/modalOpenState.ts';
 import ArticleSearchFilterModal from '@pages/article-search-filter-modal/ArticleSearchFilterModal.tsx';
-import {articleSearchFilterState} from '@recoil/articleSearchFilterState.ts';
+import { articleSearchFilterState } from '@recoil/articleSearchFilterState.ts';
 // import HomeIcon from '@assets/icon-home.svg?react';
 // import ScrappedIcon from '@assets/icon-scrapped.svg?react';
 
