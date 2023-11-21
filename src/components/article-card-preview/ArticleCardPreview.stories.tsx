5192148dc068b1639b8a3f50a06ae7415f7bb19a
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-11-22 00:28:47 +0900

    fix: 스토리북 최신화로 빌드 버그 수정

--- src/components/article-card-preview/ArticleCardPreview.stories.tsx
+++ src/components/article-card-preview/ArticleCardPreview.stories.tsx
@@ -24,11 +24,14 @@ export const Default = () => {
     headline: 'Biden’s Covid Relief Plan Is Popular. Will It Be Enough?',
     byline: 'By Giovanni Russonello',
     pub_date: '2021-02-27T10:00:09+0000',
+    url: 'https://www.nytimes.com/2021/02/27/us/politics/biden-covid-relief-plan.html',
+    glocations: [''],
+    isScrapped: true,
   };
 
   return (
     <Template>
-      <ArticleCardPreview article={defaultArticle} />
+      <ArticleCardPreview article={defaultArticle} onClickStarButton={() => {}} />
     </Template>
   );
 };
